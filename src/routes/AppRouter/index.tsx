3cd3402a2f97c18f3d5099610b5cfbdbdd416cf7
Igor Goryaynov <nomean42@interfaced.ru>
2019-07-11 16:30:12 +0700

    Get back exact

--- src/routes/AppRouter/index.tsx
+++ src/routes/AppRouter/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route , Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PrivateRoute from './PrivateRoute';
 import RootPage from '../pages/RootPage';
@@ -10,12 +10,12 @@ import ExperimentsPage from '../pages/ExperimentsPage';
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
-        <Switch>
-          <Route path="/experiments" component={ExperimentsPage} />
-          <PrivateRoute path="/" exact component={RootPage} />
-          <PrivateRoute path="/:id" exact component={TodoListPage} />
-          <PrivateRoute path="/:id/merge" exact component={MergeTodoListPage} />
-        </Switch>
+      <Switch>
+        <Route path="/experiments" exact component={ExperimentsPage} />
+        <PrivateRoute path="/" exact component={RootPage} />
+        <PrivateRoute path="/:id" exact component={TodoListPage} />
+        <PrivateRoute path="/:id/merge" exact component={MergeTodoListPage} />
+      </Switch>
     </BrowserRouter>
   );
 };
